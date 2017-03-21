3ad80d2561703766a9db00658e24815720c056e1
mswim <mswim.dev@gmail.com>
2017-03-20 18:07:13 -0600

    .gitignore update, Log in activity

--- app/src/main/java/com/mswim/gittest/MainActivity.java
+++ app/src/main/java/com/mswim/gittest/MainActivity.java
@@ -2,12 +2,15 @@ package com.mswim.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
     }
 }
